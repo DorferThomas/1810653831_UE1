a18a2ca70a39981e72bd8f183307b7d87535e8ad
DorferThomas <1810653831@stud.fh-kufstein.ac.at>
2018-11-09 17:15:51 +0100

    Aufgabe4ZweiVariablenAngelegtUndDiverseVerleichgeDurchgeführt

---
+++ src/Aufgabe4_1810653831.java
@@ -0,0 +1,22 @@
+public class Aufgabe4_1810653831 {
+    public static void main(String[] args)
+    {
+        int zahl1 = 388473;
+        int zahl2 = 99373;
+        //diverse verleiche mit den zwei angelegten Integer Zahlen (resultat ist immer true/false
+
+        System.out.println(zahl1<zahl2);
+        System.out.println(zahl2<zahl1);
+        System.out.println(zahl1>zahl2);
+        System.out.println(zahl2>zahl1);
+
+        System.out.println(zahl1<=zahl2);
+        System.out.println(zahl2<=zahl1);
+        System.out.println(zahl1>=zahl2);
+        System.out.println(zahl2>=zahl1);
+
+        System.out.println(zahl1==zahl2);
+        System.out.println(zahl1!=zahl2);
+
+    }
+}
