f97ace62c7d95f2d7227e98cf21cfcc720535ca8
DorferThomas <1810653831@stud.fh-kufstein.ac.at>
2018-11-09 17:04:29 +0100

    Aufgabe3VariablenErstelltUndDiverseRechnungenAufDerKonsoleAusgegeben

---
+++ src/Aufgabe3_1810653831.java
@@ -0,0 +1,19 @@
+public class Aufgabe3_1810653831 {
+    public static void main(String[] args)
+    {
+        int zahl1 = 2134567890;
+        int zahl2 = 49484;
+        int zahl3 = 233885855;
+        double zahl4 = 2134567890;
+        double zahl5 = 448083235;
+        double zahl6 = 4.19E-30;
+
+        System.out.println(zahl1 + zahl2);
+        System.out.println(zahl1 / zahl3);
+        System.out.println(zahl4 * zahl2);
+        System.out.println(zahl1%zahl2);
+        System.out.println(zahl3++);
+        System.out.println(zahl2--);
+        System.out.println(zahl3 + " " + zahl2);
+    }
+}
